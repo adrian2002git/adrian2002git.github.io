12734810f4376520c0b4d0d801c0a26d34eaf5d5
AdrianOrlamuende <adrian.orlamuende@gmail.com>
2023-02-28 11:43:28 +0100

    Adding Generate Champion function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,54 @@
 $(document).ready(function() {
     var oddsArray = null;
-    function getOddsArray(currentlevel) {
-        // Assuming the JSON data is stored in a variable called 'jsonData'
+
+    function getOddsArray(currentlevel, callback) {
         $.getJSON("../odds.json", function(data) {
             $.each(data.level_odds, function() {
                 var levelobj = $(this);
-                //console.log(currentlevel);
-                if(parseInt(levelobj[0]['level']) === parseInt(currentlevel)){
-                    console.log("ok!");
+                if (parseInt(levelobj[0]['level']) === parseInt(currentlevel)) {
                     oddsArray = levelobj[0]['odds'];
-                    return oddsArray;
+                    callback(oddsArray);
                 }
             });
         });
     }
-    function modifyShop() {
-            var currentlevel = $("#CurrentLevel").val();
-            var oddsArray = getOddsArray(currentlevel);
-            console.log(oddsArray);
+
+    function generateChampion(callback) {
+        var currentlevel = $("#CurrentLevel").val();
+        getOddsArray(currentlevel, function(oddsArray) {
+            var cost1odds = oddsArray[0]['percent'];
+            var cost2odds = oddsArray[1]['percent'];
+            var cost3odds = oddsArray[2]['percent'];
+            var cost4odds = oddsArray[3]['percent'];
+            var cost5odds = oddsArray[4]['percent'];
+
+            var championArray = [];
+            for (var i = 0; i < 5; i++) {
+                var random_num = Math.floor(Math.random() * 100);
+
+                if (random_num < cost1odds ) {
+                    var champion_tier = 1;
+                } else if (random_num < cost1odds + cost2odds) {
+                    var champion_tier = 2;
+                } else if (random_num < cost1odds + cost2odds + cost3odds) {
+                    var champion_tier = 3;
+                } else if (random_num < cost1odds + cost2odds + cost3odds + cost4odds) {
+                    var champion_tier = 4;
+                } else if (random_num < cost1odds + cost2odds + cost3odds + cost4odds + cost5odds) {
+                    var champion_tier = 5;
+                }
+                championArray.push(champion_tier);
+            }
+            callback(championArray);
+        });
     }
 
-
-    modifyShop();
-    console.log("ready");
+    generateChampion(function(championArray) {
+        refreshShop(championArray);
     });
+
+    function refreshShop(championArray) {
+        // Use the championArray here
+        console.log(championArray)
+    }
+});
